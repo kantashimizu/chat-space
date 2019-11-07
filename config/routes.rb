8df4a4f3410cfdb9054c94beac13a037ede199d8
kantashimizu <gantaiqingshui968@gmail.com>
2019-11-07 20:04:39 +0900

    ビュー修正２

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'messages#index'
-  resources :index
+  resources :messages, only:[:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
