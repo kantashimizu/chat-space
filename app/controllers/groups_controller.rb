0d395ebdf03d664c954066d1c5f62b42bbcb4b8e
kantashimizu <gantaiqingshui968@gmail.com>
2019-11-12 11:21:53 +0900

    edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :new]
+  before_action :set_group, only: [:edit, :update]
   def index
   end
  
@@ -31,7 +31,7 @@ class GroupsController < ApplicationController
 
 private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
