def33ac6c0c4aee79adfd330036e5394dc091372
kantashimizu <gantaiqingshui968@gmail.com>
2019-11-15 15:52:46 +0900

    aaaaaaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-server '3.112.141.13', user: 'ec2-user', roles: %w{app db web}
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
